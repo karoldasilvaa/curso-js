4d3fb3ec20d0a80723aed65c34ceb5ece8ba34db
Karol Silva <karol071298@gmail.com>
2022-08-10 13:11:49 -0300

    JS
    Condição

--- aula08/ex008.js
+++ aula08/ex008.js
@@ -1,1 +1,19 @@
-console.log('O console funcionou corretamnte')
+var vel = Number(window.prompt('Digite sua velocidade:'))
+
+if(isNaN(vel)) { 
+    alert('Apenas números')
+}
+else
+{ 
+    if (vel>60) {
+        alert('Velocidade alta')
+    }
+    else if (vel>30) {
+        alert('Velocidade normal')
+    }
+    else {
+        alert('Velocidade baixa')
+    }
+ 
+}
+
---
+++ aula08/ex009.js
@@ -0,0 +1,7 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+    if (vel > 60) {   // Condição simples
+        console.log(`Você ultapassou a velocidade permitida. MULTADO!`)
+    }
+
+console.log(`Dirija sempre usando cinto de segurança!`)
