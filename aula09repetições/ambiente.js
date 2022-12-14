aac40df8f75e2a516ce359d27396a2d3b02ac210
Karol Silva <karol071298@gmail.com>
2022-12-14 12:47:24 -0300

    gggggg
    vvvvvvvvv

--- aula09repetições/ambiente.js
+++ aula09repetições/ambiente.js
@@ -1,18 +1,23 @@
 
 /* Primeiro testa depois execulta o bloco */
 
- var k = 1
- while (k <= 6) {     /* while = enquanto */
-     console.log('Tudo bem?')
- k++ /* o k valia 1 agora está valendo 2 */
-}
+//  var k = 1
+//  while (k <= 6) {     /* while = enquanto */
+//      console.log('Tudo bem?')
+//  k++ /* o k valia 1 agora está valendo 2 */
+// }
 
 /* Primeiro execulta o bloco depois testa */
 // var k = 1
+
 // do {
 //     console.log('Tudo bem?')
 //     k++
 // } while (k <=6 )
+
+for (var x = 1; x <= 6; x++) {
+    console.log('Karol')
+}
 
 
 /*    pior jeito de fazer, nunca faça dessa maneira
@@ -24,4 +29,4 @@
    console.log('Tudo bem?')
 */
 
-
+   
